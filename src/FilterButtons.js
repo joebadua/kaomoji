f823e13bbb3e354c2e01e023428e34597df5e0a4
joebadua <joebadqq@gmail.com>
2020-01-16 21:28:16 -0800

    updated FilterButton (still working on implementation)

--- src/FilterButtons.js
+++ src/FilterButtons.js
@@ -9,40 +9,12 @@ const FilterButton = () => {
     const ifJoy = true;
     const ifLove = true;
 
-    const list = filterList.map(index => { 
-        const ButtonEvent = () => {
-
-        }
-
-        return (
-            <>
-                <Button basic color="black"
-                        className="Emoticon" 
-                        id={index.emoticon} 
-                        key={index.emoticon}
-                        size="large"
-                        onClick={ () => ButtonEvent(this)}> {index.emoticon} </Button>
-            </>
-        )
-    });
-    return (
-        <div>{list}</div>
-    )
-
-
-
 }
-
-
-
-
 
 class FilterButtons extends Component {
     render() {
         return (
             <div className="FilterButtons">
-
-                <FilterButton />
 
             </div>
         )
