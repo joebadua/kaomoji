4d9fa1079fadfe2074cb3860d9f221ec2d9e7f0b
joebadua <joebadqq@gmail.com>
2020-02-07 23:43:25 -0800

    added a new category: angry

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ class App extends Component {
   state = {
     option: "all",
     visible: true,
-    randomInt: Math.floor(Math.random() * 10) // for rendering a random emoji from the start
+    randomInt: Math.floor(Math.random() * 50) // for rendering a random emoji from the start
   }
   
   setAll = () => { this.setState({ option: "all"}) }
   setLove = () => { this.setState({ option: "love" }) }
   setJoy = () => { this.setState({ option: "joy" }) }
+  setAngry = () => {this.setState({ option: "angry"}) }
 
   render() { 
     
@@ -33,6 +34,7 @@ class App extends Component {
             <FilterButtons setOption={this.setAll} filter={"All"} icon={"circle"}/>
             <FilterButtons setOption={this.setJoy} filter={"Joy"} icon={"smile outline"}/>
             <FilterButtons setOption={this.setLove} filter={"Love"} icon={"heart"}/>
+            <FilterButtons setOption={this.setAngry} filter={"Angry"} icon={"thumbs down outline"}/>
           </h1>
           <EmojiButton option={this.state.option}/>
       </div>
